6da1c9053c953e021fedc61e8a29f8f17a98e2d2
DNA601 <dnavarrete6001@gmail.com>
2022-07-06 15:06:32 -0700

    data is displayed

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+var townName = document.getElementById('town')
+var citySearchName = document.getElementById('')
+var searchBtn = document.getElementById('searchBtn')
+var searchInput = document.getElementById('searchInput')
 var apikey = "954c644fba0f209e01b1e8c05e53de98"
+    //api key
+var temp = document.getElementById('temp')
+var humidity = document.getElementById('humidity')
+var wind = document.getElementById('wind')
+
+//ids from the html
 
 
-
-document.querySelector('.btn').addEventListener('click', function() {
-    var cityName = document.querySelector('.grab').value
+searchBtn.addEventListener('click', function() {
+    var cityName = searchInput.value
     console.log(cityName)
+        //Search button works when clicked
 
     var url2 = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + apikey
     fetch(url2)
@@ -16,15 +26,20 @@ document.querySelector('.btn').addEventListener('click', function() {
             var lat = res[0].lat
             var lon = res[0].lon
 
-            var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apikey
+            var url = "https://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=" + lat + "&lon=" + lon + "&appid=" + apikey
             fetch(url)
                 .then(function(res) {
                     return res.json()
                 })
                 .then(function(data) {
                     console.log(data)
+                    townName.innerHTML = cityName
+                    temp.innerHTML = "Temp: " + data.current.temp + "°F"
+                    humidity.innerHTML = " Humidity " + data.current.humidity + " % "
+                    wind.innerHTML = " Wind: " + data.current.wind_speed + "mph"
+                        //Api data displays
 
                 })
-
         })
+
 })
