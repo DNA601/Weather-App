6c803ce6d57225b7778f43d8c6f9d1174670ac16
DNA601 <dnavarrete6001@gmail.com>
2022-07-06 19:15:46 -0700

    daily

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var humidity = document.getElementById('humidity')
 var wind = document.getElementById('wind')
 var uvi = document.getElementById('uvi')
 var bar = document.getElementById('bar')
+var forecastSection = document.getElementById('forecast-section')
 
 var searchHistory = []
 
@@ -38,17 +39,32 @@ searchBtn.addEventListener('click', function() {
 
 
             var url = "https://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=" + lat + "&lon=" + lon + "&appid=" + apikey
+                //where i grab my data from. 
             fetch(url)
                 .then(function(res) {
                     return res.json()
                 })
                 .then(function(data) {
                     console.log(data)
+                    renderForecast(data.daily)
                     townName.innerHTML = cityName
                     temp.innerHTML = "Temp: " + data.current.temp + "°F"
                     humidity.innerHTML = " Humidity " + data.current.humidity + " % "
                     wind.innerHTML = " Wind: " + data.current.wind_speed + "mph"
                     uvi.innerHTML = "UVI: " + data.current.uvi
+                    if (data.current.uvi <= 2) {
+                        uvi.classList.add('green')
+
+                    } else if (data.current.uvi <= 5) {
+                        uvi.classList.add('yellow')
+                    } else if (data.current.uvi <= 7) {
+                        uvi.classList.add('orange')
+                    } else if (data.current.uvi <= 10) {
+                        uvi.classList.add('red')
+                    } else {
+                        uvi.classList.add('pink')
+                    }
+
 
 
 
@@ -70,3 +86,18 @@ function renderHistory() {
 
 }
 renderHistory()
+
+function renderForecast(daily) {
+    //function to display daily forecast. 
+    forecastSection.innerHTML = ``
+    for (let i = 1; i <= 5; i++) {
+        console.log(daily[i])
+        const forecastCard = document.createElement('div')
+        forecastCard.innerHTML = `
+            <h3>${new Date(daily[i].dt * 1000)}</h3>
+            <p>Temperature: ${daily[i].temp.day}</p>
+        `
+            //inner html work shortcut
+        forecastSection.appendChild(forecastCard)
+    }
+}
